33d257a89eca1bcb45af95850378ad3a5bd6db8e
contagiousmind <sebmann@gmail.com>
2025-10-14 18:34:32 +0100

    Starting

--- BandSite.js
+++ BandSite.js
@@ -1,8 +1,13 @@
 // JavaScript source code
 $(function () {
-
+    LoadMenu();
 
     
 });
 
 
+function LoadMenu() {
+    $( "#Menu" ).load( "Menu.html", function() {
+        alert( "Load was performed." );
+    });
+}
