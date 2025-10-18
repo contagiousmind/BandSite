4276b2d6187a6fce86dc76b21ce0db7b191bea21
contagiousmind <sebmann@gmail.com>
2025-10-18 11:08:30 +0100

    Main image size fun

--- BandSite.js
+++ BandSite.js
@@ -4,6 +4,45 @@ $(function () {
     
 });
 
+var lastSrollY = 0;
+var startingImageSize = 0;
+
+// main image is full screen on load, then size on scroll down
+window.onscroll = function (e) {
+
+    // are we going up or down?...
+    var downUp = 'down';
+    var nowScrollY = window.scrollY;  // Value of scroll Y in px
+
+    if (nowScrollY < lastSrollY) {
+        // downUp = 'up';
+    }
+    lastSrollY = nowScrollY;
+
+    // set the height of images to be minus how much they've scrolled...
+    var currentImageHeight = $("#MainLogo").height();
+    if (startingImageSize == 0) {
+        startingImageSize = currentImageHeight;
+    }
+
+    // new height...
+    var newHeight = currentImageHeight - (nowScrollY / 120);
+    // if we're going up, add it back..
+    if (downUp == 'up') {
+        newHeight = currentImageHeight + (nowScrollY / 120);
+    }
+
+    // but we can't scroll up - as it's at the top of the page... so account for this...
+    var toAdd = startingImageSize - newHeight;
+
+    if (newHeight > 50 && newHeight <= startingImageSize) {
+        // set height of image...
+        $("#MainLogo").height(newHeight);
+
+        // $("body").css({"padding-top": toAdd});
+
+    }
+};
 
 
 function MenuItem_Click(tab) {
