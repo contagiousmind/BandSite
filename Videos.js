b7313fd99e80584d3b65f22b3fa96fbcac8b7c85
contagiousmind <sebmann@gmail.com>
2025-10-18 16:32:24 +0100

    More video page updates

--- Videos.js
+++ Videos.js
@@ -2,19 +2,22 @@
 var videoList = new Array();
 videoList.push(new YoutubeVideo(
       'czhJQ-_EJwE'
-    , '\'Barking at Ghosts\' Live @Hunstanton Battle of the Bands 2025'
+    , 'Barking at Ghosts'
+    , ' Live @Hunstanton Battle of the Bands 2025'
     , 'wide'
 ));
 
 videoList.push(new YoutubeVideo(
       'GP6FhKt3C4U'
-    , '\'Strange by my Side\' Live @The Tram Depot - August 2025'
+    , 'Strange by my Side'
+    , ' Live @The Tram Depot - August 2025'
     , 'wide'
 ));
 
 videoList.push(new YoutubeVideo(
       'iKNnCNSaMDk'
-    , '\'Better Than Before\' Live @The Tram Depot - August 2025'
+    , 'Better Than Before'
+    , ' Live @The Tram Depot - August 2025'
     , 'wide'
 ));
 
@@ -28,6 +31,7 @@ function BuildVideos() {
 
     for(i = 0; i < videoList.length; i++) {
         html += template.replace(/\$VIDEOTITLE\$/g, videoList[i].Title)
+                        .replace(/\$SONGNAME\$/g, videoList[i].SongName)
                         .replace(/\$VIDEOID\$/g, videoList[i].ID)
                         .replace(/\$DISPLAYSIZE\$/g, videoList[i].DisplaySize)
                     ;
@@ -39,8 +43,9 @@ function BuildVideos() {
 }
 
 
-function YoutubeVideo(id, title, displaySize) {
+function YoutubeVideo(id, songName, title, displaySize) {
     this.ID = id;
+    this.SongName = songName;
     this.Title = title;
     this.DisplaySize = displaySize;
 }
