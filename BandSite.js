da3facff400b75f261159fbc8e35312022f27f40
contagiousmind <sebmann@gmail.com>
2025-10-25 10:27:52 +0100

    Clean up

--- BandSite.js
+++ BandSite.js
@@ -48,45 +48,45 @@ var loadedTabs = new Array();
 
 
 // this was fun to do, but useless!
-var lastSrollY = 0;
-var startingImageSize = 0;
+// var lastSrollY = 0;
+// var startingImageSize = 0;
 
-// main image is full screen on load, then size on scroll down
-window.onscroll = function (e) {
+// // main image is full screen on load, then size on scroll down
+// window.onscroll = function (e) {
 
-    // are we going up or down?...
-    var downUp = 'down';
-    var nowScrollY = window.scrollY;  // Value of scroll Y in px
+//     // are we going up or down?...
+//     var downUp = 'down';
+//     var nowScrollY = window.scrollY;  // Value of scroll Y in px
 
-    if (nowScrollY < lastSrollY) {
-        // downUp = 'up';
-    }
-    lastSrollY = nowScrollY;
+//     if (nowScrollY < lastSrollY) {
+//         // downUp = 'up';
+//     }
+//     lastSrollY = nowScrollY;
 
-    // set the height of images to be minus how much they've scrolled...
-    var currentImageHeight = $("#MainLogo").height();
-    if (startingImageSize == 0) {
-        startingImageSize = currentImageHeight;
-    }
+//     // set the height of images to be minus how much they've scrolled...
+//     var currentImageHeight = $("#MainLogo").height();
+//     if (startingImageSize == 0) {
+//         startingImageSize = currentImageHeight;
+//     }
 
-    // new height...
-    var newHeight = currentImageHeight - (nowScrollY / 10);
-    // if we're going up, add it back..
-    if (downUp == 'up') {
-        newHeight = currentImageHeight + (nowScrollY / 10);
-    }
+//     // new height...
+//     var newHeight = currentImageHeight - (nowScrollY / 10);
+//     // if we're going up, add it back..
+//     if (downUp == 'up') {
+//         newHeight = currentImageHeight + (nowScrollY / 10);
+//     }
 
-    // but we can't scroll up - as it's at the top of the page... so account for this...
-    var toAdd = startingImageSize - newHeight;
+//     // but we can't scroll up - as it's at the top of the page... so account for this...
+//     var toAdd = startingImageSize - newHeight;
 
-    if (newHeight > 50) { //  || newHeight <= startingImageSize) {
-        // set height of image...
-        // $("#MainLogo").height(newHeight);
+//     if (newHeight > 50) { //  || newHeight <= startingImageSize) {
+//         // set height of image...
+//         // $("#MainLogo").height(newHeight);
 
-        // $("body").css({"padding-top": toAdd});
+//         // $("body").css({"padding-top": toAdd});
 
-    }
-};
+//     }
+// };
 
 
 function MenuItem_Click(tab) {
@@ -149,39 +149,6 @@ function MenuItem_Click(tab) {
 
 
 
-// found a way to ready from a google sheet?...
-// https://github.com/jsoma/tabletop?tab=readme-ov-file
-// https://github.com/jsoma/tabletop/blob/master/examples/simple/no-tabletop.html
-// cors issues of course...
-
-// https://github.com/mholt/PapaParse/issues/809
-// to help cors errors? nope
-/*
-window.googleDocCallback = function () { 
-    return true; 
-};
-
-
-
-function GetData(url) {
-        Papa.parse(url, {
-          download: true,
-          header: true,
-          complete: GetData_Complete
-        })
-}
-
-function GetData_Complete() {
-    var data = results.data;
-
-    alert("Successfully processed " + data.length + " rows!")
-
-
-}
-
-*/
-
-// try this?
 function GetData(sheetName, completeEvent) {
 
     if (loadedTabs.includes(sheetName)) {
@@ -193,7 +160,7 @@ function GetData(sheetName, completeEvent) {
     // read site from url to get sheet id?\
     var aaa = ''; 
     var spreadsheetId = ''
-    if (location.host == 'tpap') {
+    if (location.host == 'contagiousmind.github.io') {
         aaa = 'AIzaSyAjynx0jJxrinbDqmsVFEA3cCmKEDC9NG0';
         spreadsheetId = '17D3wVbnIUR5WS0LpTRUniBTU9rGuXmbWuq94i2GmRPQ';
 
@@ -221,7 +188,6 @@ function GetData(sheetName, completeEvent) {
     // var sheetName = 'Sheet1'; // Replace with your sheet name
     var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${aaa}`;
 
-
     Ajax(url, function(data) {
 
         // keep track of what we've loaded.
@@ -229,7 +195,6 @@ function GetData(sheetName, completeEvent) {
 
         completeEvent.call('', data);
         
-
     }, '');
 }
 
@@ -246,18 +211,6 @@ function Ajax(url, completeEvent, args) {
             completeEvent.call(this, data);
         }
     });
-
-
-    // fetch(url)
-    // .then(
-    //     response => response.json()
-    // )
-    // // .then(
-    // //     data => console.log(data.values)
-    // // )
-    // .catch(
-    //     error => console.error('Error:', error)
-    // );
 
 }
 
