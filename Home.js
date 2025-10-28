2b75a2d4b527b8010ef89ac3c53f3b564a2f0e98
contagiousmind <sebmann@gmail.com>
2025-10-28 21:16:41 +0000

    favicon fix

--- Home.js
+++ Home.js
@@ -96,7 +96,7 @@ function SetFavIcon() {
         link.rel = 'icon';
         document.head.appendChild(link);
     }
-    link.href = '/' + aboutUs.BandName.toLowerCase().replace(/ /g, '') + '_favicon.ico';
+    link.href = location.href + aboutUs.BandName.toLowerCase().replace(/ /g, '') + '_favicon.ico';
     
 }
 
